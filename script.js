dd2de1a95d7a9d69f0a71b4ef237246a9fc4bcd2
Jen Ignacio <jignacio2204@gmail.com>
2020-11-10 19:04:03 -0500

    added carousel js and theme

--- script.js
+++ script.js
@@ -8,31 +8,50 @@ function formatQueryParams(params) {
     return queryItems.join('&');
   }
 
-function displayUpcoming(responseJson){
-console.log(responseJson);
-}
+      //Add 90 days from beginning date for upcomingGames function
+      Date.prototype.addDays = function(days) {
+        let date = new Date(this.valueOf());
+        date.setDate(date.getDate() + days);
+        return date;
+    }
 
+function cardCheck(responseJson){
+  
+}
 
 function displayTitleSearch(responseJson){
     $("#search-results").empty();
 
+    let cardCheck = checkIfInAnyList(responseJson);
+
     responseJson.results.map(game => {
-        $("#search-results").append(`<li>${game.name}</li>`)
+        console.log(game.developers);
+        $("#search-results").append(`
+        <li><div id="${game.slug}" class="card">
+            <header class="card-header"><img src="./images/icon_addCard"></header>
+            <div class='card-info'>
+                <p class='card-name'>${game.name}</p>
+                <p class='pub-name'>${game.publishers}</p>
+            </div>
+        </div></li>
+        `)
     })
 
     if(responseJson.previous !== null){
-        $("#search-results").append(`<div id='prevPage'>PREV</div>`)
+        $("#search-results").append(`<button class='btn prevBtn' id='prevPage'>PREV</button>`)
         $("#prevPage").click(function(){
             getPrev(responseJson.previous);
         })
     }
 
     if(responseJson.next !== null){
-        $("#search-results").append(`<div id='nextPage'>NEXT</div>`);
+        $("#search-results").append(`<button class='btn nextBtn' id='nextPage'>NEXT</button>`);
         $("#nextPage").click(function(){
             getNext(responseJson.next);
         })
     }
+
+    $("#results").css("display", "block");
 }
 
 function getNext(nextPage){
@@ -52,7 +71,7 @@ function getTitle(gametitle){
     const params = {
         key: apiKey,
         search: gametitle,
-        page_size: 10
+        page_size: 20
     };
 
     const queryString = formatQueryParams(params);
@@ -66,21 +85,79 @@ function getTitle(gametitle){
       })
 }
 
-function getUpcomingGames(){
-    let newGames = [];
+const displayUpcomingSection = function(){
+return (`        <!--UPCOMING GAMES NEXT MONTH-->
+<section id="upcoming">
+    <h2>Top 10 Upcoming Games</h2>
+    <h4>Add these games to your wishlist!</h4>
+    <div class="js-upcoming-list" id="upcoming-list">
+    </div>
+</section>`)
+}
 
-    let setdate = new Date;
-    let month = setdate.setMonth(setdate.getMonth()+1);
-    console.log(Date.valueOf());
+const displayUpcoming = function(responseJson){
 
-    console.log(setdate);
+    responseJson.results.map((e, idx) =>{
+        //convert release date to MM/DD/YYYY format
+        let inputDate = new Date(e.released);
+        let formattedDate = (inputDate.getMonth() + 1) + "/" + inputDate.getDate() + "/" + inputDate.getFullYear();
 
-    let pullDate = setdate.toJSON().split("T");
-    console.log(pullDate[0])
+        $("#upcoming-list").append(`<div class="upcoming-card" id="${e.slug}" style="background:url('${e.background_image}');background-size:cover;background-position:center;"><div class='card-text'><p>${e.name}</p><p>Release Date: ${formattedDate}</p></div></div>`);
+    });
+
+    $('.js-upcoming-list').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+}
+
+
+function getUpcoming(){
+
+    let beginDate = new Date;    
+    let endDate = new Date().addDays(90);
+
+    let beginDay = beginDate.toJSON().split("T");
+    let endDay = endDate.toJSON().split("T");
+
+    let dateRange = beginDay[0] + "," + endDay[0];
 
     const params = {
         key: apiKey,
-        dates: setdate
+        ordering: "-rating",
+        dates: dateRange,
+        page_size: 10
     };
 
     const queryString = formatQueryParams(params);
@@ -93,21 +170,84 @@ function getUpcomingGames(){
         }
         throw new Error(response.statusText);
       })
-      .then(responseJson => newGames.push(responseJson))
+      .then(responseJson => displayUpcoming(responseJson))
       .catch(err => {
         $('#js-error-message').text(`Something went wrong: ${err.message}`);
       })
+}
 
-      return newGames;
+const displayMenuBoxes = function(){
+    return (`<section id="menu-boxes">
+    <!--WISHLIST-->
+    <div id="wishlist" class="main-menu-item">
+        <p>WISHLIST</p>
+        <ul id="js-wishlist-list"></ul>
+    </div>
+
+    <!--COLLECTIONS-->
+    <div id="collection" class="main-menu-item">
+        <p>COLLECTIONS</p>
+        <ul id="js-collection-list"></ul>
+    </div>
+    </section>`);
+}
+
+
+const displaySearch = function(){
+    return (`<!--SEARCH BAR-->
+    <section id="search-bar">
+        <form id="search-games">
+            <label for="search">ENTER TITLE:</label><br>
+            <input id="search" type="text" name="gametitle">
+
+            <input id="searchBtn" form="search-games" type="submit" class="btn search-btn" value="SEARCH">
+        </form>
+        <div id="results" class="hidden">
+            <h2>Search Results</h2>
+            <ul id="search-results"></ul>
+        </div>
+    </section>`)
+}
+
+const displayList = function(type){
+  $(".list").append(`
+    <h3>${type}</h3>
+    <ul></li>
+  `);
+}
+
+function getList(listButton){
+
+    $(".main-menu-item").removeClass("selected");
+    $("#"+listButton).addClass("selected");
+
+    $("#upcoming").remove();
+    $(".list").remove();
+
+    displayList(listButton);
+
+    return($("#menu-boxes").after(`<section class="list" id="${listButton}-list">
+    </section>`));
 }
 
 function render(){
+
+    getUpcoming();
+    $("body")
+    .append(displayMenuBoxes)
+    .append(displayUpcomingSection)
+    .append(displaySearch);
+    
     $("form#search-games").submit(event =>{
         event.preventDefault();
 
         let gametitle = $("#search").val();
 
         getTitle(gametitle);
+    });
+
+    $(".main-menu-item").click(function(){
+        getList(this.id);
     });
 }
 
